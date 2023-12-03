e2e5135fc37984d3a3e8853f0ac2c4e11a1d0cde
JM <jmjanzen@gmail.com>
2023-12-03 16:49:06 -0500

    2023 day 2 1/2 (gottem)
    That one wasn't so bad. :)

--- 2023-12-02/game.rb
+++ 2023-12-02/game.rb
@@ -9,42 +9,29 @@ class Game
   def initialize(id:, parts:)
     @id = id
     @parts = parts.map { |part| Part.new(part) }
-
-    # XXX Oops, we want to check for possible sets, not totals, duh
-    @totals = { 'red' => 0, 'blue' => 0, 'green' => 0 }
-    @parts.each do |part|
-      @totals['red'] += part.cubes['red'] || 0
-      @totals['blue'] += part.cubes['blue'] || 0
-      @totals['green'] += part.cubes['green'] || 0
-    end
   end
 
   def possible?()
     # "only 12 red cubes, 13 green cubes, and 14 blue cubes"
+    is_possible = true
+    @parts.each do |part|
+      if (part.cubes['green'] || -1) > 13
+        is_possible = false
+        break
+      elsif (part.cubes['blue'] || -1) > 14
+        is_possible = false
+        break
+      elsif (part.cubes['red'] || -1) > 12
+        is_possible = false
+        break
+      end
+    end
 
-    # XXX Well now there are just no possibles :/ ?
-    if @totals['red'] > 12
-      puts "too many red"
-      false
-    end
-    if @totals['blue'] > 14
-      puts "too many blue"
-      false
-    end
-    if @totals['green'] > 13
-      puts "too many green"
-      false
-    end
-
-    true
+    is_possible
   end
 
   def to_s
-    { id: @id, parts: @parts, totals: @totals }.to_s
-    """
-    id: #{@id} #{self.possible? ? 'OK' : 'IMPOSSIBLE' }
-    totals: #{@totals}
-    """
+    """id: #{@id} #{self.possible? ? 'OK' : 'IMPOSSIBLE' }"""
   end
 end
 
@@ -53,7 +40,6 @@ end
 class Part
   attr_accessor :cubes
   def initialize(s)
-    #puts "\t#{s}"
     # TODO Merge into one obj
     # Coming out [{"red"=>1}, {"blue"=>1}, {"green"=>4}]
     # instead of single obj
@@ -65,7 +51,7 @@ class Part
       .reduce({}, &:merge)
   end
 
-  #def to_s
-  #  @cubes
-  #end
+  def to_s
+    """part: #{@cubes.to_s}"""
+  end
 end
